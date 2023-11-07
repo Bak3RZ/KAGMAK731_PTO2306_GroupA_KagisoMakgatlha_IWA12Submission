c4e977333519459634adca4303657527c107bd3b
Kagiso <101631291+Bak3RZ@users.noreply.github.com>
2023-11-07 18:51:59 +0200

    Update script.js
    Challenge1
    Update02

--- script.js
+++ script.js
@@ -57,18 +57,19 @@ const books = [ {
 
 books[0].html.checkinButtons.style.color = '' // Must remain undefined
 books[0].html.status.style.color = STATUS_MAP.status.color; // We use STATUS_MAP.status to access the properties of the specific status of the book.
-books[0].html.reserve.disabled = !STATUS_MAP.status.canReserve;
-books[0].html.checkoutButtons.disabled = !STATUS_MAP.status.canCheckout; //we use the ! operator to reverse the logic because canReserve, canCheckout, and canCheckIn are set to true if the action is allowed, and we want to disable the button if it's not allowed.
-books[0].html.checkinButtons.disbaled = !STATUS_MAP.status.canCheckIn;
+books[0].html.reserve.disabled = STATUS_MAP.status.canReserve ? 'enabled' : 'disabled'
+books[0].html.checkoutButtons.disabled = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+books[0].html.checkinButtons.disbaled = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
 
 books[1].html.checkinButtons.style.color = '' // Must remain undefined
 books[1].html.status.style.color = STATUS_MAP.status.color;
-books[1].html.reserve.disabled = !STATUS_MAP.status.canReserve;
-books[1].html.checkoutButtons.disabled = !STATUS_MAP.status.canCheckout;
-books[1].html.checkinButtons.disbaled = !STATUS_MAP.status.canCheckIn;
+books[1].html.reserve.disabled = STATUS_MAP.status.canReserve ? 'enabled' : 'disabled'
+books[1].html.checkoutButtons.disabled = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+books[1].html.checkinButtons.disbaled = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
 
 books[2].html.checkinButtons.style.color = '' // Must remain undefined
 books[2].html.status.style.color = STATUS_MAP.status.color;
-books[2].html.reserve.disabled = !STATUS_MAP.status.canReserve;
-books[2].html.checkoutButtons.disabled = !STATUS_MAP.status.canCheckout;
-books[2].html.checkinButtons.disbaled = !STATUS_MAP.status.canCheckIn;
+books[2].html.reserve.disabled = STATUS_MAP.status.canReserve ? 'enabled' : 'disabled'
+books[2].html.checkoutButtons.disabled = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+books[2].html.checkinButtons.disbaled = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+
